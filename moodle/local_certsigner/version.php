7128dfccfbe6cd46d5d6123ba39ec2bee9084293
Ubuntu <ubuntu@vps-e3c6445b.vps.ovh.us>
2026-08-07 17:53:39 +0000

    release: v2.0.0 Moodle plugin update

--- moodle/local_certsigner/version.php
+++ moodle/local_certsigner/version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_certsigner';
-$plugin->version   = 2026080700;
+$plugin->version   = 2026080702;
 $plugin->requires  = 2022041900;
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.1.0';
+$plugin->release   = '2.0.0';
